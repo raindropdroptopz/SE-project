832fb8a11541f5866b0dedbf1128f3ce68b89f36
oatopn <oat_opn@hotmail.com>
2025-09-16 15:23:56 +0700

    first commit

--- Project/server.js
+++ Project/server.js
@@ -84,21 +84,3 @@ dbInit.connect((err) => {
         });
     });
 });
-app.post('/api/login', (req, res) => {
-    const { username, password } = req.body;
-    const sql = 'SELECT * FROM users WHERE username = ? AND password = ?';
-    db.query(sql, [username, password], (err, results) => {
-        if (err) {
-            return res.status(500).json({ success: false, message: 'เกิดข้อผิดพลาดในระบบ' });
-        }
-        if (results.length > 0) {
-            res.json({ success: true });
-        } else {
-            res.status(401).json({ success: false, message: 'ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง' });
-        }
-    });
-});
-
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
-});
